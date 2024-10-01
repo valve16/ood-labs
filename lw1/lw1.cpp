4e6ae50c9a1227719e78b9318334531dbad8b5b4
valve16 <glushkovoleg2004@yandex.ru>
2024-10-01 12:36:22 +0300

    add SFML

---
+++ lw1/lw1.cpp
@@ -0,0 +1,30 @@
+﻿#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+int main()
+{
+
+    RenderWindow window(VideoMode(400, 400), L"Новый проект", Style::Default);
+
+    window.setVerticalSyncEnabled(true);
+
+    CircleShape shape(100.f, 3);
+    shape.setPosition(100, 100);
+    shape.setFillColor(Color::Magenta);
+
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+
+        window.clear(Color::Blue);
+        window.draw(shape);
+        window.display();
+    }
+    return 0;
+}
